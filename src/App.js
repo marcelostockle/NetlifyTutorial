0247ae6a7f8aa8212c2ff61524f9908e7d1cf00e
marcelostockle <marcelostockle@hotmail.com>
2022-10-28 21:25:16 -0300

    Boilerplate React code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className="links">
+        <span className="links-item">
+          <a href="https://github.com/marcelostockle/NetlifyTutorial">GitHub Repository</a>
+        </span>
+      </div>
+      <div className="header">
+        <span className="header-title">Netlify Tutorial</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;

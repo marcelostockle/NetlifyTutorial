c39344d02be88897c7c033f73a68bef39828c180
marcelostockle <marcelostockle@hotmail.com>
2022-11-20 01:23:33 -0300

    UPDATE get-products

--- netlify/functions/get-products/get-products.js
+++ netlify/functions/get-products/get-products.js
@@ -1,20 +1,40 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const { MongoClient } = require("mongodb")
-
-const mongoClient = new MongoClient(process.env.MONGODB_URI)
-const clientPromise = mongoClient.connect()
+const axios = require("axios")
 
 const handler = async (event) => {
-  try {
-    const database = (await clientPromise).db("dalila")
-    const collection = database.collection("Product")
-    const results = collection.find({}).sort({ category:1 }).toArray()
-    return {
-      statusCode: 200,
-      body: JSON.stringify(results),
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+  const axios = require('axios')
+  const data = JSON.stringify({
+    "collection": "Product",
+    "database": "dalila",
+    "dataSource": "Cluster0",
+    "sort": { "category": 1 }
+  })
+          
+  const config = {
+    method: 'post',
+    url: 'https://data.mongodb-api.com/app/data-zwswv/endpoint/data/v1/action/find',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': process.env.MONGODB_APIKEY,
+    },
+    data: data
+  }
+          
+  axios(config)
+    .then(function (response) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response.data),
+      }
+    })
+    .catch(function (error) {
+      return { statusCode: 500, body: error.toString() }
+    });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
   }
 }
 

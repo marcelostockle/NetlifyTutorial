83bfaf5791b6e90574c26e5627de711212121990
marcelostockle <marcelostockle@hotmail.com>
2022-11-19 23:55:39 -0300

    Add function: get-products

---
+++ netlify/functions/get-products/get-products.js
@@ -0,0 +1,21 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const { MongoClient } = require("mongodb")
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI)
+const clientPromise = mongoClient.connect()
+
+const handler = async (event) => {
+  try {
+    const database = (await clientPromise).db("dalila")
+    const collection = database.collection("Product")
+    const results = collection.find({}).toArray()
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
